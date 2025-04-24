5460b73682681f4512d600ba6cdd477192b3485f
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-04-24 15:51:41 +0200

    Added comments

--- transaction.py
+++ transaction.py
@@ -2,16 +2,22 @@ import random
 
 random.seed(0)
 
-"""
-    makeTransaction(maxValue=3) -> dict
+
+def makeTransaction(maxValue=3):
+    """
     Creates a random transaction between Alice and Bob, where the amount is between -maxValue and maxValue.
     The sum of deposits and withdrawals must be 0 (tokens are neither created nor destroyed)
     A user’s account must have sufficient funds to cover any withdrawals.
     The transaction is represented as a dictionary with account names as keys and amounts as values.
     The function returns a dictionary with the transaction details.
     The transaction is guaranteed to be valid according to the rules defined in the isValidTxn function.
-"""
-def makeTransaction(maxValue=3):
+
+    Args:
+        maxValue (int, optional): Defaults to 3.
+
+    Returns:
+        str: A string representation of the transaction.
+    """
     sign = int(random.getrandbits(1)) * 2 - 1
     # Randomly choose a value between -1 and 1
     amount = random.randint(1, maxValue)
@@ -21,6 +27,7 @@ def makeTransaction(maxValue=3):
     # By construction, this will always return transactions that respect the conservation of tokens.
     # However, note that we have not done anything to check whether these overdraft an account
     return {u'Alice':alicePays,u'Bob':bobPays}
+
 
 # This is a list of transactions that will be used to test the algorithm.
 txnBuffer = [makeTransaction() for i in range(30)]
