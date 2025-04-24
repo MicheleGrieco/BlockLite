adce2d5954eacc0349900737fcbd3206453b66f0
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-04-24 17:59:02 +0200

    refactor

--- blocklite.py
+++ blocklite.py
@@ -1,10 +1,71 @@
+import hashlib
+import json
+import sys
+import random
+
+random.seed(0)
+
+
+def hashMe(msg=""):
+    """
+    Hashes a message using SHA-256 algorithm.
+    The message can be a string or a dictionary.
+    If it's a dictionary, it will be converted to a JSON string with sorted keys to ensure consistent hashing.
+
+    Args:
+        msg (str, optional): Defaults to "".
+
+    Returns:
+        str: The SHA-256 hash of the message.
+    """
+    if type(msg) != str:
+        # Sorting keys to guarantee consistent hashing for dicts
+        msg = json.dumps(msg, sort_keys=True)
+    
+    # Python 2 and 3 compatibility
+    # In Python 2, str is a byte string, so we can hash it directly
+    # In Python 3, str is a unicode string, so we need to encode it to bytes first
+    # Note: In Python 3, str is unicode, and bytes is a separate type
+    if sys.version_info.major == 2:
+        return hashlib.sha256(msg).hexdigest()
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
+
+
+def makeTransaction(maxValue=3):
+    """
+    Creates a random transaction between Alice and Bob, where the amount is between -maxValue and maxValue.
+    The sum of deposits and withdrawals must be 0 (tokens are neither created nor destroyed)
+    A user’s account must have sufficient funds to cover any withdrawals.
+    The transaction is represented as a dictionary with account names as keys and amounts as values.
+    The function returns a dictionary with the transaction details.
+    The transaction is guaranteed to be valid according to the rules defined in the isValidTxn function.
+
+    Args:
+        maxValue (int, optional): Defaults to 3.
+
+    Returns:
+        str: A string representation of the transaction.
+    """
+    sign = int(random.getrandbits(1)) * 2 - 1
+    # Randomly choose a value between -1 and 1
+    amount = random.randint(1, maxValue)
+    alicePays = sign * amount
+    bobPays = -1 * alicePays
+    
+    # By construction, this will always return transactions that respect the conservation of tokens.
+    # However, note that we have not done anything to check whether these overdraft an account
+    return {u'Alice':alicePays,u'Bob':bobPays}
+
+
+# This is a list of transactions that will be used to test the algorithm.
+txnBuffer = [makeTransaction() for i in range(30)]
+
 """
     RULES for a simple banking system:
     - The sum of deposits and withdrawals must be 0 (tokens are neither created nor destroyed)
     - A user’s account must have sufficient funds to cover any withdrawals
 """
-import json
-import hashcode as hashMe
 
 def updateState(txn, state):
     """
